c8c4dda78c93ce1c1f10549cb0b8c50287a83444
Rafael Marçal <rafaelmarcal2103@gmail.com>
2024-08-13 10:56:23 -0300

    Firebase Configurado

--- app/src/main/java/rafaelmarcal/ifsp/edu/quizaedes/MainActivity.java
+++ app/src/main/java/rafaelmarcal/ifsp/edu/quizaedes/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
